74cf52b67048ed79685df726e3db1564e3712831
Igor Rurkowski <93292404+TheNoxy@users.noreply.github.com>
2023-01-23 14:52:47 +0100

    Add files via upload

---
+++ data.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+char date;
+int dd, mm, rrrr;
+cout << "Wpisz dzien: ";
+cin >> dd;
+cout << "Wpisz miesiac: ";
+cin >> mm;
+cout << "Wpisz rok:";
+cin >> rrrr;
+cout << "Data to: ";
+cout << "" << dd;
+cout << "-" << mm;
+cout << "-" << rrrr;
+
+}
